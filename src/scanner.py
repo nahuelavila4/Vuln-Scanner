7998ae127ba991a0978d9b3beadbe4ea2ed5f8cd
nahuel <avilanahuel99@gmail.com>
2024-11-07 12:24:12 -0300

    1.5 Scan certificates

--- src/scanner.py
+++ src/scanner.py
@@ -2,12 +2,16 @@ import requests
 import socket
 import ssl
 import re
+from datetime import datetime
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def scanner():
-    domain = input("Enter the server address: ")
+    dom = input("Enter the server address: ")
     try:
-        res = requests.get(domain)
-        head = res.headers()
+        res = requests.get(dom, verify=False)
+        head = res.headers
         auth_data = {'Content-Security-Policy': head.get('Content-Security-Policy', 'Missing'),
             'Strict-Transport-Security': head.get('Strict-Transport-Security', 'Missing'),
             'X-Content-Type-Options': head.get('X-Content-Type-Options', 'Missing'),
@@ -16,15 +20,29 @@ def scanner():
         print(f"Security headers status \n")
         for header, status in auth_data.items():
             print(f"{header}: {status}")
-        check_https(domain)
+        check_https("localhost", 8080)
     except Exception as e:
         print(f"Error: {e}")
 
-def check_https(domain):
-    print("als")
-    cert = ssl.create_defaul_context() #Manage security configuration
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((domain, 443))
+def check_https(domain, port):
+    context = ssl.create_default_context() #Manage security configuration
+    context.check_hostname = False  # Ignorar la verificación del nombre
+    context.verify_mode = ssl.CERT_NONE  # No verificar el certificado
+
+    with socket.create_connection((domain, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=domain) as ssock:
+            cert = ssock.getpeercert() #Retrieve SSL certificate
+            print(cert['NotAfter'])
+            '''
+            exp_date = cert['NotAfter']
+            exp_obj = datetime.strptime(exp_date, "%b %d %H:%M:%S %Y GMT")
+            act_date = datetime.now()
+            if exp_obj > act_date:
+                print("The SSL certificate has expired")
+            else:
+                print("The SSL certificate is valid")
+            '''
+    return cert
 
 if __name__ == "__main__":
     scanner()
